02ff48196bf0aef0501e911733a2fb1668d00945
mtpa94 <myathetpaing94@gmail.com>
2021-08-17 12:38:08 +0630

    - add bg music

--- THA-bdprojectMTPA/js/app.js
+++ THA-bdprojectMTPA/js/app.js
@@ -1,6 +1,7 @@
 const clickbtn = document.getElementById('btn-click');
-const cardcontainer = document.getElementById('card-container')
-const additionalimg = document.getElementById('additional-img')
+const cardcontainer = document.getElementById('card-container');
+const additionalimg = document.getElementById('additional-img');
+var audio = new Audio('./music/BDSong.mp3');
 
 clickbtn.addEventListener('click', ()=>{
     // console.log('hay');
@@ -8,13 +9,13 @@ clickbtn.addEventListener('click', ()=>{
         clickbtn.innerText = 'CLOSE WISH';
         cardcontainer.style.opacity = 1;
         additionalimg.style.opacity = 0;
-        // console.log(clickbtn.innerText);
+        audio.play();
     } else
     if (clickbtn.innerText === 'CLOSE WISH'){
         clickbtn.innerText = 'OPEN TO SEE WISH';
         additionalimg.style.opacity = 1;
         cardcontainer.style.opacity = 0;
-        // console.log(clickbtn.innerText);
+        audio.pause();
     }
 
 });
